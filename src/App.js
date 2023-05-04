96478c7dfa2cc44677e8db6f20915b5f03706693
NgHQHuy <nguyenhoangquanghuy2000@gmail.com>
2023-05-04 20:15:39 +0700

    pl

--- src/App.js
+++ src/App.js
@@ -29,7 +29,7 @@ function handleInClickMenu() {
 
 function App() {
   return (
-    <Router>
+    <Router basename="/Portfolio-BasicReactJS">
       <div className="App">
         <header className="App-header">
           {/* <img src={logo} className="App-logo" alt="logo" />
